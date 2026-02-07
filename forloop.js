d14e06d653fa74861505d0826de3448ed902e10d
msd131102 <shivmodernz@gmail.com>
2026-02-07 20:32:53 +0530

    Even Countdown" Challenge for loop

--- forloop.js
+++ forloop.js
@@ -58,3 +58,56 @@ for(let i=1;i<=10;i++){
     total = total + i;
 }
 console.log(total)
+
+// // // Problem 2: The Countdown
+
+// // // Write a loop that counts backwards from 5 down to 1, and then prints the word "Blastoff!" at the very end.
+
+// // // Hint: Start with let i = 5.
+
+// // // Hint: Use i-- to go down.
+
+// // // Hint: Put the "Blastoff" message outside and after the loop's curly braces {}.
+
+
+let m;
+
+for(m=5;m>=1;m--){
+    console.log(m);
+}
+
+console.log("Blastoff");
+
+
+
+
+// The "Skip Countdown" Challenge
+// Write a loop that counts down from 20 to 0, but instead of counting by 1, it counts down by 5.
+
+// Goal: Your output should be: 20 15 10 5 0 Lift off!
+
+// Condition: Use i >= 0 to make sure the loop includes the number 0.
+
+// Increment/Decrement: Use i -= 5 to subtract 5 each time.
+
+let n;
+
+for(n=20;n>=0;n -= 5){
+    console.log(n);
+}
+console.log("Lift off!");
+
+
+// The "Even Countdown" Challenge
+// Write a loop that starts at 10 and counts down to 1. Inside the loop, use an if condition to only console.log() the number if it is Even.
+
+// Goal: Your output should be: 10 8 6 4 2
+
+// Hint: Start at 10, use i-- to go down by 1, and use if (i % 2 === 0) inside.
+
+
+let f;
+for(f=10;f>=1; f--){
+    if(f % 2 === 0)
+    console.log(f);
+}
